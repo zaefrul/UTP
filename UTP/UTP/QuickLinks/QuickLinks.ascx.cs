f9fdf2ab85592892a1a30d63dd20d746fdb199bf
zaefrul <zaefrul@gmail.com>
2018-10-22 17:56:06 +0800

    QuickLink Issue
    Fixed quick link issue when retrieving the content

--- UTP/UTP/QuickLinks/QuickLinks.ascx.cs
+++ UTP/UTP/QuickLinks/QuickLinks.ascx.cs
@@ -37,13 +37,7 @@ namespace UTP.QuickLinks
         {
             try
             {
-                var q = new SPQuery()
-                {
-                    Query = @"<Where><Eq><FieldRef Name='Active' /><Value Type='Boolean'>1</Value></Eq></Where><OrderBy><FieldRef Name='Category_x003a_Sequence' Ascending='True' /><FieldRef Name='Sequence' Ascending='True' /></OrderBy>",
-                    ViewFields = @"  <FieldRef Name='URL' /><FieldRef Name='Comments' /><FieldRef Name='Category' /><FieldRef Name='Category_x003a_Section' />"
-                };
                 SPWeb Web = new SPSite(SiteCollection).OpenWeb();
-                SPListItemCollection Items = Web.Lists[ListName].GetItems(q);
                 StringBuilder stringBuilder1 = new StringBuilder();
                 StringBuilder stringBuilder2 = new StringBuilder();
                 StringBuilder stringBuilder3 = new StringBuilder();
@@ -51,32 +45,49 @@ namespace UTP.QuickLinks
                 string _tempCategory, CurrentCategory = "";
                 bool _sec1, _sec2, _sec3, _sec4;
                 _sec1 = _sec2 = _sec3 = _sec4 = false;
-                foreach (SPListItem Item in Items)
-                {
-                    StringBuilder stringBuilder = new StringBuilder();
-                    _tempCategory = Item[Helper.QLCategoryField].ToString().Contains("#") ? Item[Helper.QLCategoryField].ToString().Split('#')[1] : Item[Helper.QLCategoryField].ToString();
-                    int sec = Convert.ToInt32(Convert.ToDouble(Item[Helper.QLSectionField].ToString().Contains("#") ? Item[Helper.QLSectionField].ToString().Split('#')[1] : Item["Category_x003a_Section"].ToString()));
-                    switch (sec)
-                    {
-                        case 1: stringBuilder = stringBuilder1; _sec1 = true; break;
-                        case 2: stringBuilder = stringBuilder2; _sec2 = true; break;
-                        case 3: stringBuilder = stringBuilder3; _sec3 = true; break;
-                        case 4: stringBuilder = stringBuilder4; _sec4 = true; break;
-                    };
-                    if (_tempCategory != CurrentCategory)
-                    {
-                        if (CurrentCategory != "")
-                        {
-                            stringBuilder.Append("</ul>");
-                        }
-                        CurrentCategory = _tempCategory;
-                        stringBuilder.Append("<div class='link-category'>"+CurrentCategory+"</div>");
-                        stringBuilder.Append("<ul>");
-                    }
-                    SPFieldUrlValue Url = new SPFieldUrlValue(Item[Helper.QLUrlField].ToString());
-                    stringBuilder.Append("<li><a href = '"+Url.Url+"' >"+Url.Description+"</a></li>");
-                }
 
+                //Grabbing The Category
+                SPList list = Web.Lists[new Guid("e4318186-6255-40dd-8951-98ee4365b77d")]; // List for QuickLink Category
+                var qUery = new SPQuery()
+                {
+                    Query = @"<Where><Eq><FieldRef Name='Active' /><Value Type='Boolean'>1</Value></Eq></Where><OrderBy><FieldRef Name='Sequence' Ascending='True' /></OrderBy>"
+                };
+                var r = list.GetItems(qUery);
+                foreach (SPListItem cItem in r)
+                {
+                    var q = new SPQuery()
+                    {
+                        Query = $"<Where><And><Eq><FieldRef Name='Active' /><Value Type='Boolean'>1</Value></Eq><Eq><FieldRef Name='Category' /><Value Type='Lookup'>{cItem["Title"].ToString()}</Value></Eq></And></Where><OrderBy><FieldRef Name='Sequence' Ascending='True' /></OrderBy>",
+                        ViewFields = @"  <FieldRef Name='URL' /><FieldRef Name='Comments' /><FieldRef Name='Category' /><FieldRef Name='Category_x003a_Section' />"
+                    };
+                    SPListItemCollection Items = Web.Lists[ListName].GetItems(q);
+
+                    foreach (SPListItem Item in Items)
+                    {
+                        StringBuilder stringBuilder = new StringBuilder();
+                        _tempCategory = Item[Helper.QLCategoryField].ToString().Contains("#") ? Item[Helper.QLCategoryField].ToString().Split('#')[1] : Item[Helper.QLCategoryField].ToString();
+                        int sec = Convert.ToInt32(Convert.ToDouble(Item[Helper.QLSectionField].ToString().Contains("#") ? Item[Helper.QLSectionField].ToString().Split('#')[1] : Item["Category_x003a_Section"].ToString()));
+                        switch (sec)
+                        {
+                            case 1: stringBuilder = stringBuilder1; _sec1 = true; break;
+                            case 2: stringBuilder = stringBuilder2; _sec2 = true; break;
+                            case 3: stringBuilder = stringBuilder3; _sec3 = true; break;
+                            case 4: stringBuilder = stringBuilder4; _sec4 = true; break;
+                        };
+                        if (_tempCategory != CurrentCategory)
+                        {
+                            if (CurrentCategory != "")
+                            {
+                                stringBuilder.Append("</ul>");
+                            }
+                            CurrentCategory = _tempCategory;
+                            stringBuilder.Append("<div class='link-category'>" + CurrentCategory + "</div>");
+                            stringBuilder.Append("<ul>");
+                        }
+                        SPFieldUrlValue Url = new SPFieldUrlValue(Item[Helper.QLUrlField].ToString());
+                        stringBuilder.Append("<li><a href = '" + Url.Url + "' >" + Url.Description + "</a></li>");
+                    }
+                }
                 QuickLink1.Text = stringBuilder1.ToString();
                 QuickLink2.Text = stringBuilder2.ToString();
                 QuickLink3.Text = stringBuilder3.ToString();
